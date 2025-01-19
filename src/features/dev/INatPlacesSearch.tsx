97ac7b890b63af0ce739815fe6f1780172a5da6f
Steven Granter <stevengranter@gmail.com>
2025-01-19 18:37:21 -0330

    Add controlled Autocomplete component for INatPlacesSearch using debounced value for fetching

--- src/features/dev/INatPlacesSearch.tsx
+++ src/features/dev/INatPlacesSearch.tsx
@@ -1,3 +1,43 @@
+import { useQuery } from "@tanstack/react-query"
+import { API_SERVER } from "~/features/_shared/api/constants.ts"
+import { useEffect, useState } from "react"
+import { useDebouncedValue } from "@mantine/hooks"
+import { Autocomplete } from "@mantine/core"
+
+type ApiResponse = {
+  results?: Array<{ uuid: string; name: string }>
+}
+
 export default function INatPlacesSearch() {
-  return "INatPlacesSearch"
+  const [value, setValue] = useState("")
+  const [debouncedValue] = useDebouncedValue(value, 500)
+
+  const {
+    data: responseData,
+    error,
+    isLoading,
+  } = useQuery<ApiResponse>({
+    queryKey: [API_SERVER.INAT, `/places/autocomplete?q=`, debouncedValue],
+    enabled: !!value,
+  })
+
+  useEffect(() => {
+    console.log(value)
+  }, [value])
+
+  return (
+    <Autocomplete
+      label="Place"
+      placeholder="Begin typing name for results"
+      value={value}
+      onChange={(value) => setValue(value)}
+      data={
+        responseData && !isLoading && !error && responseData.results
+          ? responseData.results.map((result) => {
+              return { value: result.uuid, label: result.name }
+            })
+          : []
+      }
+    ></Autocomplete>
+  )
 }
